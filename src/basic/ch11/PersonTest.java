4bb185648708e8b38c1c551f5f5e4f355b432f74
ning00060 <ning00060@gmail.com>
2024-04-18 10:11:54 +0900

    this의 3가지사용법

---
+++ src/basic/ch11/PersonTest.java
@@ -0,0 +1,23 @@
+package basic.ch11;
+
+public class PersonTest {
+
+	public static void main(String[] args) {
+
+		Person person1 = new Person("홍길동", 100);
+		Person personBox = person1;
+		Person personBox2 = person1.getPerson();
+		Person personLee = new Person("이순신", 101);
+		System.out.println("-------------------");
+		// 위 코드 까지는 Heap 메모리 영역에 객체가 2개 성성된 상태이다.
+		// 지역변수인 person1,personBox,personBox2 는 같은 객체를 가리킨다
+
+		// 연습 문제 -
+		// setName 메서드 만들어 주세요.
+
+		personBox2.setName("티모");
+		person1.showInfo();
+
+	}
+
+}
